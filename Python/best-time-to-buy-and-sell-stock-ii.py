8176cff7d4dd68b50b5369328aee5899f03715f4
crowdy kim <crowdy@gmail.com>
2017-08-28 23:19:32 +0900

    Update best-time-to-buy-and-sell-stock-ii.py

--- Python/best-time-to-buy-and-sell-stock-ii.py
+++ Python/best-time-to-buy-and-sell-stock-ii.py
@@ -27,3 +27,30 @@ class Solution:
 if __name__ == "__main__":
     result = Solution().maxProfit([3, 2, 1, 4, 2, 5, 6])
     print result
+
+"""
+1. Can I think for a second?
+2. Think loud
+[1, 2, 1, 5, 9, 6, 8] 이 있다면,
+2-1 + 5-1 + 9-5 + 8-6 = 3 + 4 + 4 + 2 = 13
+
+그냥 내 전의 값보다 크다면 그 차이를 더하면 될 것 같다.
+
+3. I can have a few more examples
+[4, 3, 2, 1] 
+[1, 1, 1, 1]
+
+4. Does it seem like a good strategy?
+
+함수 맥스프라핏(가격배열)
+    결과 = 0
+    1 부터 가격배열 길이만큼 반복
+        나의 가격이 하나 전의가격 보다 크다면
+            결과 += 나의 가격 - 하나 전의 가격
+    리턴 결과
+
+---
+
+나와 하나 전의 가격을 보면 되므로 맵함수를 사용하면 될 것 같다.
+
+"""
