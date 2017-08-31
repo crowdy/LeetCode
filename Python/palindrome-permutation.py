256a29290675d41ccbda24577a7685b83baf860b
crowdy kim <crowdy@gmail.com>
2017-08-31 22:16:57 +0900

    Update palindrome-permutation.py

--- Python/palindrome-permutation.py
+++ Python/palindrome-permutation.py
@@ -8,3 +8,25 @@ class Solution(object):
         :rtype: bool
         """
         return sum(v % 2 for v in collections.Counter(s).values()) <  2
+
+"""
+
+Write an efficient function that checks whether any permutation 
+of an input string is a palindrome.
+You can assume the input string only contains lowercase letters.
+
+Examples:
+
+"civic" should return true
+"ivicc" should return true
+"civil" should return false
+"livci" should return false
+
+
+1. Can I think for a second?
+2. Think loud
+
+각 알파벳을 세어서, 전부 짝수개로 되어 있거나, 하나만 홀수개 이면 되는 거죠
+
+
+"""
